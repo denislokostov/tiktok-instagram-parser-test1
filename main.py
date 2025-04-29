9a4fe9a0f1d9123e417d1d6d828b818a1373b2db
Denis Lókostov <85484561+denislokostov@users.noreply.github.com>
2025-04-29 21:38:55 +0500

    Update main.py
    добавлен Flask для запуска сервера

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Сервер запущен — парсер TikTok и Instagram готов к настройке.")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Сервер запущен — парсер TikTok и Instagram готов к настройке."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
